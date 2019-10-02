f58f4da3bdbaed87e81a00ad01c07cb94e65b1af
Emelia Blankenship <egblip@gmail.com>
2019-10-02 11:44:37 -0400

    Updated spacing
    Important improvements

--- NowPlaying/SpotifyCallController.swift
+++ NowPlaying/SpotifyCallController.swift
@@ -4,49 +4,39 @@
 //
 
 import Foundation
-
 class SpotifyCallController {
-
   func getCurrentTrackProperty(property: String) -> String? {
-
     let call = "get " + property + " of current track"
     let output = spotifyCall(command: call)
-
     if(output != nil) {
       return output!
     } else {
       return nil;
     }
   }
-
   func getCurrentPlayingStatus() -> SpotifyState? {
-
     let call = "get player state"
     let output = spotifyCall(command: call)
-
     if(output == "kPSP") {
       return SpotifyState.playing
     } else if(output == "kPSp") {
       return SpotifyState.paused
+      // Very nice maximizing of the runtime
     } else {
       return nil;
     }
   }
-
   func spotifyCall(command: String) -> String? {
-
     let fullCommand = "if application \"Spotify\" is running then tell application \"Spotify\" to " + command
-
     let commandObject = NSAppleScript(source: fullCommand)
     var error: NSDictionary?
-
     let output: NSAppleEventDescriptor = commandObject!.executeAndReturnError(&error)
-
     if(output.stringValue != nil) {
       return output.stringValue!
     } else {
       return nil
     }
+    // Its hacktober in here
   }
 
 }
